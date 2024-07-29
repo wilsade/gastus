f618cf4038a5153966fcf4d1429a5db23c384666
William Sade De Paiva <sade@totvs.com.br>
2024-07-29 13:09:29 -0300

    - Renomear projeto; - Definir modelo para duas categorias

--- api/gastus-api/Controllers/CategoriaController.cs
+++ api/gastus-api/Controllers/CategoriaController.cs
@@ -18,10 +18,38 @@ namespace Gastus.Api.Controllers
     {
       try
       {
-        var obj = new
+        var obj = new[]
         {
-          codigo = 1,
-          nome = "Lazer"
+          new
+          {
+            id = 1,
+            nome = "Alimentação",
+            filhas = new[]
+            {
+              new { idCategoria = 1, id = 1, nome = "Doces" },
+              new { idCategoria = 1, id = 2, nome = "Lanchonete" },
+              new { idCategoria = 1, id = 3, nome = "Padaria" },
+              new { idCategoria = 1, id = 4, nome = "Restaurante" },
+              new { idCategoria = 1, id = 5, nome = "Supermercado" },
+              new { idCategoria = 1, id = 6, nome = "App celular" },
+              new { idCategoria = 1, id = 7, nome = "Outros" },
+            }
+          },
+          new
+          {
+            id = 2,
+            nome = "Lazer",
+            filhas = new[]
+            {
+              new { idCategoria = 2, id = 1, nome = "Cinema" },
+              new { idCategoria = 2, id = 2, nome = "Dança" },
+              new { idCategoria = 2, id = 3, nome = "Jogos" },
+              new { idCategoria = 2, id = 4, nome = "Festas" },
+              new { idCategoria = 2, id = 5, nome = "Bar" },
+              new { idCategoria = 2, id = 6, nome = "Viagem" },
+              new { idCategoria = 2, id = 7, nome = "Outros" },
+            }
+          }
         };
         return Ok(obj);
       }
