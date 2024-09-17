ee2770c84a6c7f8699f8af7071d8e14b30507dc6
wilsade <wilsade@gmail.com>
2024-09-17 13:40:24 -0300

    Update CategoriasController.cs

--- api/gastus-api/Controllers/CategoriasController.cs
+++ api/gastus-api/Controllers/CategoriasController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+
+using System.Data.SQLite;
 
 namespace Gastus.Api.Controllers
 {
@@ -9,6 +12,8 @@ namespace Gastus.Api.Controllers
   [Route("[controller]")]
   public class CategoriasController : GastusBaseController
   {
+    const string DATABASE_FILE_NAME = @"Data Source=gastus.db;Version=3;";
+
     /// <summary>
     /// Recuperar todas as categorias cadastradas
     /// </summary>
@@ -18,6 +23,35 @@ namespace Gastus.Api.Controllers
     {
       try
       {
+        // Estabelecer a conexão com o banco de dados
+        using (var connection = new SQLiteConnection(DATABASE_FILE_NAME))
+        {
+          connection.Open();
+
+          // Executar a consulta para obter as categorias
+          var query = "SELECT id, nome FROM Categoria";
+          using (var command = new SQLiteCommand(query, connection))
+          {
+            var categorias = new List<object>();
+
+            using (var reader = command.ExecuteReader())
+            {
+              while (reader.Read())
+              {
+                var categoria = new
+                {
+                  id = reader.GetInt32(0),
+                  nome = reader.GetString(1)
+                };
+
+                categorias.Add(categoria);
+              }
+            }
+
+            return Ok(categorias);
+          }
+        }
+
         var obj = new[]
         {
           new
@@ -52,6 +86,37 @@ namespace Gastus.Api.Controllers
           }
         };
         return Ok(obj);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest(ex);
+      }
+    }
+
+    [HttpPost()]
+    public IActionResult AddCategoria([FromBody] CategoriaModel categoria)
+    {
+      try
+      {
+        // Estabelecer a conexão com o banco de dados
+        using (var connection = new SQLiteConnection(DATABASE_FILE_NAME))
+        {
+          connection.Open();
+
+          int idCategoria = 1; // categoria["id"].ToObject<int>();
+          string nomeCategoria = "Alimentção"; // categoria["nome"].ToString();
+
+          // Executar a consulta para obter as categorias
+          var query = "INSERT INTO Categoria (id, nome) VALUES (@id, @nome)";
+          using (var command = new SQLiteCommand(query, connection))
+          {
+            command.Parameters.AddWithValue("@id", categoria.Id);
+            command.Parameters.AddWithValue("@nome", categoria.Nome);
+            command.ExecuteNonQuery();
+          }
+
+          return Ok();
+        }
       }
       catch (Exception ex)
       {
