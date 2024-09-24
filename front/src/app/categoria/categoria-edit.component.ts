9700aa6276971c58fe0cfd959683d19daaa7d5be
William Sade de Paiva <wilsade@msn.com>
2024-09-24 13:35:05 -0300

    Definir table para subcategorias

--- front/src/app/categoria/categoria-edit.component.ts
+++ front/src/app/categoria/categoria-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { PoGridRowActions, PoModalAction, PoModalComponent, PoModule } from '@po-ui/ng-components';
+import { PoModalAction, PoModalComponent, PoModule, PoTableColumn } from '@po-ui/ng-components';
 import { ICategoria } from '../_models/ICategoria';
 import { CategoriaService } from './categoria.service';
 import { FormsModule } from '@angular/forms';
@@ -32,9 +32,9 @@ export class CategoriaEditComponent {
     }
   }
 
-  protected colunas: Array<any> = [
-    { label: 'Id', property: 'Id', readonly: true },
-    { label: 'Nome', property: 'Nome', required: true, width: '300' },
+  protected colunas: Array<PoTableColumn> = [
+    { label: 'Id', property: 'Id', width: '10%' },
+    { label: 'Nome', property: 'Nome' },
   ]
 
   @ViewChild('modal')
@@ -50,14 +50,6 @@ export class CategoriaEditComponent {
   onConfirmouModal = new EventEmitter<ICategoria>();
 
   protected meuTitulo: string;
-
-  protected rowActions: PoGridRowActions = {
-    beforeSave: this.onBeforeSave.bind(this),
-    afterSave: this.onAfterSave.bind(this),
-    beforeRemove: this.onBeforeRemove.bind(this),
-    afterRemove: this.onAfterRemove.bind(this),
-    beforeInsert: this.onBeforeInsert.bind(this)
-  };
 
   exibirModal(item: ICategoria): void {
     this.meuTitulo = `Editar categoria: ${item.Nome}`;
@@ -79,33 +71,8 @@ export class CategoriaEditComponent {
     });
   }
 
-  private onBeforeSave(row: any, old: any) {
-    if (!row.Nome)
-      alert('Nome da subcategoria obrigatório')
+  protected inserirSubCategoria_Click(): void {
+    console.log('inserirSubCategoria_Click');
 
-    const achou = this.categoria.SubCategorias.find(x => x.Nome == row.Nome);
-    console.log('achou', achou);
-
-    return row.Nome;
-  }
-
-  private onAfterSave(row: any) {
-    console.log('onAfterSave(new): ', row);
-  }
-
-  private onBeforeRemove(row: any) {
-    console.log('onBeforeRemove: ', row);
-
-    return true;
-  }
-
-  private onAfterRemove() {
-    console.log('onAfterRemove: ');
-  }
-
-  private onBeforeInsert(row: any) {
-    console.log('onBeforeInsert: ', row);
-
-    return true;
   }
 }
