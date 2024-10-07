62531d1da9f78fb9d736e4970ab34d5cfa3598d0
wilsade <wilsade@gmail.com>
2024-10-07 13:04:18 -0300

    Importar BrowserAnimationsModule

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,19 +1,18 @@
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { PoHttpRequestModule } from '@po-ui/ng-components';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importar apenas animações se necessário
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom([PoHttpRequestModule]),
+    importProvidersFrom([PoHttpRequestModule, BrowserAnimationsModule]), // Se precisar de animações
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withInterceptorsFromDi())
-  ],
-  
+  ]
 };
