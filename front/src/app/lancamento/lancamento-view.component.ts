e9c07c35bfe017eb4b20328886d6af6820e864ac
wilsade <wilsade@gmail.com>
2024-10-07 13:32:37 -0300

    Definição de colunas para lançamentos

--- front/src/app/lancamento/lancamento-view.component.ts
+++ front/src/app/lancamento/lancamento-view.component.ts
@@ -1,26 +1,38 @@
-import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
-import { PoModule, PoNotificationService } from '@po-ui/ng-components';
+import { PoModule, PoNotificationService, PoTableColumn } from '@po-ui/ng-components';
 import { GastusBaseComponent } from '../shared/gastus-base-component';
 import { ILancamento } from '../_models/ILancamento';
 import { LancamentoService } from './lancamento.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lancamento-view',
   standalone: true,
-  imports: [PoModule, CommonModule],
+  imports: [CommonModule, PoModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './lancamento-view.component.html'
 })
 export class LancamentoViewComponent extends GastusBaseComponent implements OnInit {
 
   constructor(protected override _notification: PoNotificationService,
-    private _service: LancamentoService) {
+    private readonly _service: LancamentoService) {
     super(_notification);
   }
 
   loading = false;
   lancamentos: ILancamento[] = [];
+
+  colunas: PoTableColumn[] = [
+    this.createColumnId(false),
+    { label: 'Data', property: 'Data', type: 'date', format: 'dd/MM/yyyy' },
+    { label: 'Título', property: 'Titulo' },
+    { label: 'Comentário', property: 'Comentario' },
+    { label: 'Categoria', property: 'NomeCategoria' },
+    { label: 'SubCategoria', property: 'NomeSubCategoria' },
+    { label: 'Tipo', property: 'NomeTipoTransacao' },
+    { label: 'Valor', property: 'Valor', type: 'number', format: '1.2-5' },
+    { label: 'Valor2', property: 'Valor', type: 'cellTemplate' },
+  ]
 
   ngOnInit(): void {
     this.carregarLancamentos();
@@ -40,5 +52,17 @@ export class LancamentoViewComponent extends GastusBaseComponent implements OnIn
         this.loading = false;
       }
     })
+  }
+
+  protected formatValue(item: number): string {
+    const formattedValue = item.toFixed(2).replace('.', ',');
+    const parts = formattedValue.split(',');
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    const formattedWithThousands = parts.join(',');
+
+    if (item < 0)
+      return `(${formattedWithThousands.replace('-', '')})`;
+    else
+      return formattedWithThousands;
   }
 }
