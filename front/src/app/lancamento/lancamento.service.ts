3b699dcc111cf4540fdd8e67c98686685b30f610
wilsade <wilsade@gmail.com>
2024-10-08 13:25:09 -0300

    coluna SALDO

--- front/src/app/lancamento/lancamento.service.ts
+++ front/src/app/lancamento/lancamento.service.ts
@@ -15,14 +15,24 @@ export class LancamentoService {
   getEmptyLancamento(): ILancamento {
     return {
       Id: 0, Data: new Date(), Titulo: '', Comentario: '', IdCategoria: 0, IdSubCategoria: 0,
-      IdTipoTransacao: null, Valor: 0
+      IdTipoTransacao: null, Valor: 0, SALDO: 0
     }
   }
 
   getLancamentos(): Observable<ILancamento[]> {
-    return this._http.get<ILancamento>(this.lancamentosUrl).pipe(
-      map((response: any) => {
+    return this._http.get<ILancamento[]>(this.lancamentosUrl).pipe(
+      map((response: ILancamento[]) => {
+        this.calculaSaldo(response);
+        console.log(response);
         return response;
       }));
+  }
+
+  private calculaSaldo(lancamentos: ILancamento[]): void {
+    let saldo = 0;
+    lancamentos.forEach(lancamento => {
+      saldo += lancamento.Valor;
+      lancamento.SALDO = saldo;
+    });
   }
 }
