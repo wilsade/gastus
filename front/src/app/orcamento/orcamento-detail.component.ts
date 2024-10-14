1dd1bd65040ccbb4c19ab59541b835c8aa85185e
wilsade <wilsade@gmail.com>
2024-10-14 13:08:18 -0300

    getMeses

--- front/src/app/orcamento/orcamento-detail.component.ts
+++ front/src/app/orcamento/orcamento-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { PoModalComponent, PoModule, PoNotificationService } from '@po-ui/ng-components';
+import { PoModalComponent, PoModule, PoNotificationService, PoSelectOption } from '@po-ui/ng-components';
 import { GastusBaseComponent } from '../shared/gastus-base-component';
 import { IOrcamento } from '../_models/IOrcamento';
 import { OrcamentoService } from './orcamento.service';
@@ -22,10 +22,16 @@ export class OrcamentoDetailComponent extends GastusBaseComponent {
   @ViewChild('modal')
   modal: PoModalComponent;
 
+  meses: PoSelectOption[] = this.getMeses();
   orcamento: IOrcamento = this._service.getEmptyOrcamento();
 
   showEditmodal(item: IOrcamento): void {
     this.orcamento = item;
     this.modal.open();
+  }
+
+  protected alterouComboMes(numMes: number): void {
+    this.orcamento.NumMes = numMes;
+    this.orcamento.NomeMes = this.meses.find(m => m.value === numMes)?.label ?? '';
   }
 }
