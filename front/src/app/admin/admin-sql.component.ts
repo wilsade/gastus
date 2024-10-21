4bd657274bb745f8b281901d586acd9ca2d2fb48
wilsade <wilsade@gmail.com>
2024-10-21 12:40:05 -0300

    Remoção de textarea

--- front/src/app/admin/admin-sql.component.ts
+++ front/src/app/admin/admin-sql.component.ts
@@ -2,14 +2,13 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { PoModule, PoNotificationService } from '@po-ui/ng-components';
 
 import { GastusBaseComponent } from '../shared/gastus-base-component';
-import { PoCodeEditorModule } from '@po-ui/ng-code-editor';
 import { AdminService } from './admin.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-sql',
   standalone: true,
-  imports: [PoModule, PoCodeEditorModule, FormsModule],
+  imports: [PoModule, FormsModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './admin-sql.component.html'
 })
@@ -20,11 +19,31 @@ export class AdminSqlComponent extends GastusBaseComponent {
     super(_notification);
   }
 
-  protected codigo = '';
+  readonly QUERY = 'Consultar';
+  readonly EXECTUCE = 'Executar';
+
+  protected codigoSQL = '';
+  protected labelBotao = this.QUERY;
+  protected botaoDesabilitado = true;
   protected query = [];
 
+  protected alterouCodigoSQL(): void {
+    this.labelBotao = this.QUERY;
+    if (!this.codigoSQL || this.codigoSQL.length <= 3) {
+      this.botaoDesabilitado = true;
+      return;
+    }
+    if (this.codigoSQL.toLowerCase().startsWith('select')) {
+      this.labelBotao = this.QUERY;
+      this.botaoDesabilitado = false;
+    } else {
+      this.labelBotao = this.EXECTUCE;
+      this.botaoDesabilitado = false;
+    }
+  }
+
   protected queryClick(): void {
-    this._service.query(this.codigo).subscribe({
+    this._service.query(this.codigoSQL).subscribe({
       next: data => {
         this.query = data;
       },
