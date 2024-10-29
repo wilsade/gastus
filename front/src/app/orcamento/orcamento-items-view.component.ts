982c0e921ac84e0f5195a26c88a0634c68543225
wilsade <wilsade@gmail.com>
2024-10-29 12:46:46 -0300

    Exclusão de orçamento

--- front/src/app/orcamento/orcamento-items-view.component.ts
+++ front/src/app/orcamento/orcamento-items-view.component.ts
@@ -5,16 +5,21 @@ import { IOrcamento, IOrcamentoView } from '../_models/IOrcamento';
 import { CommonModule } from '@angular/common';
 import { ColunaValorComponent } from '../shared/coluna-valor.component';
 import { OrcamentoDetailComponent } from './orcamento-detail.component';
+import { InputDialogService } from '../shared/input-dialog.service';
+import { OrcamentoService } from './orcamento.service';
 
 @Component({
   selector: 'app-orcamento-items-view',
   standalone: true,
   imports: [PoModule, CommonModule, ColunaValorComponent, OrcamentoDetailComponent],
+  providers: [InputDialogService],
   templateUrl: './orcamento-items-view.component.html'
 })
 export class OrcamentoItemsViewComponent extends GastusBaseComponent {
 
-  constructor(protected override _notification: PoNotificationService) {
+  constructor(protected override _notification: PoNotificationService,
+    private readonly _dlgModal: InputDialogService,
+    private readonly _service: OrcamentoService) {
     super(_notification);
   }
 
@@ -35,8 +40,40 @@ export class OrcamentoItemsViewComponent extends GastusBaseComponent {
   ]
 
   protected acoesTabela: PoTableAction[] = [
-    { label: 'Editar', icon: this.iconeEditar, action: this.abrirModalParaEdicao.bind(this) }
+    { label: 'Editar', icon: this.iconeEditar, action: this.abrirModalParaEdicao.bind(this) },
+    {
+      label: 'Excluir', icon: this.iconeEditar, action: (item: any) => {
+        const msg = 'Atenção!<br>' +
+          'Deseja realmente excluir o orçamento?<br>' +
+          `<strong>Identificador: </strong> ${item.Id}<br>` +
+          `<strong>Categoria: </strong> ${item.NomeCategoria}<br>` +
+          `<strong>SubCategoria: </strong> ${item.NomeSubCategoria}<br>` +
+          `<strong>Valor: </strong> ${item.Valor}`;
+
+        this._dlgModal.confirm({
+          message: msg,
+          title: 'Exclusão de orçamento',
+          confirm: () => this.excluirOrcamento(item)
+        });
+      }
+    }
   ]
+
+  private excluirOrcamento(item: IOrcamento): void {
+    this._service.excluirOrcamento(item.Id).subscribe({
+      next: data => {
+        if (data > 0) {
+          this.orcamento.Items = this.orcamento.Items.filter(i => i.Id != item.Id);
+          this._notification.success({ message: 'Orçamento excluído com sucesso!', duration: 1500 });
+          this.onFechouEdicao.emit();
+        }
+      },
+      error: err => {
+        this.tratarErro(err);
+      },
+      complete: () => { }
+    });
+  }
 
   private abrirModalParaEdicao(item: IOrcamento): void {
     this.modalDetail.showEditmodal(item);
