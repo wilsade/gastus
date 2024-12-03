d9ffe4339de7685026c14aaee6a86d0a7e8d7584
wilsade <wilsade@gmail.com>
2024-12-03 12:36:34 -0300

    Relatório de total das aplicações

--- api/Gastus.Core/RelatoriosRepository.cs
+++ api/Gastus.Core/RelatoriosRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dapper;
 
 using Gastus.Domain;
 
@@ -13,5 +9,31 @@ namespace Gastus.Core
   /// </summary>
   public class RelatoriosRepository(string databaseFileName) : GastusBaseRepository(databaseFileName), IRelatoriosRepository
   {
+    /// <summary>
+    /// Recuperar o total das aplicações
+    /// </summary>
+    /// <returns>Total aplicações</returns>
+    public List<TotalAplicacaoModel> GetTotalAplicacoes()
+    {
+      var connection = GetConnection(false);
+      const string sql = @"
+        SELECT 
+            'Conta corrente' AS Nome, SUM(L.Valor) AS Valor, 0 AS Ordem
+        FROM Lancamento L
+
+        UNION
+
+        SELECT A.Nome AS Nome, LA.Valor AS Valor, 1 AS Ordem
+        FROM Aplicacao A
+        JOIN 
+            LancamentoAplicacao LA ON A.Id = LA.IdAplicacao
+        WHERE 
+            LA.Data = (SELECT MAX(LA2.Data)
+                FROM LancamentoAplicacao LA2
+                WHERE LA2.IdAplicacao = LA.IdAplicacao)
+        ORDER BY  Ordem,  Nome;";
+      var result = connection.Query<TotalAplicacaoModel>(sql).ToList();
+      return result;
+    }
   }
 }
