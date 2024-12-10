3e2ba079d1f7792a383b3256e52c3a781da6f0f5
wilsade <wilsade@gmail.com>
2024-12-10 13:29:42 -0300

    Lançamentos da categoria

--- front/src/app/_models/IRelatorios.ts
+++ front/src/app/_models/IRelatorios.ts
@@ -2,5 +2,16 @@ export interface ILancamentosPorCategoriaMes {
   NomeMes: string;
   Total: number;
   TOTAL_FORMATADO: string;
-  Categorias: any[];
+  Categorias: IReportCategoria[];
+}
+
+export interface IReportCategoria extends ILancamentosPorCategoriaMes {
+  Lancamentos: ILancamentoDeCategoria[];
+}
+
+export interface ILancamentoDeCategoria {
+  Codigo: number;
+  Nome: string;
+  Valor: number;
+  VALOR_FORMATADO: string;
 }
